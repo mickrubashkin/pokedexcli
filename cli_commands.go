695c13e9089eaa8f03af90d90264a25439c29946
Mick Ruabshkin <mikhail.rubashkin@gmail.com>
2026-01-08 13:44:52 +0100

    feat(repl): add support for "exit" and "help" commands

---
+++ cli_commands.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println()
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+
+	for _, command := range getCommands() {
+		fmt.Printf("%v: %v\n", command.name, command.description)
+	}
+	fmt.Println()
+
+	return nil
+}
--- repl.go
+++ repl.go
@@ -19,7 +19,17 @@ func startRepl() {
 		}
 
 		commandName := words[0]
-		fmt.Printf("Your command was: %v\n", commandName)
+		command, ok := getCommands()[commandName]
+		if ok {
+			err := command.callback()
+			if err != nil {
+				fmt.Println(err)
+			}
+			continue
+		} else {
+			fmt.Println("Unknown command, use 'help' to see available commands")
+			continue
+		}
 	}
 }
 
@@ -28,4 +38,25 @@ func cleanInput(text string) []string {
 	words := strings.Fields(lowered)
 
 	return words
+}
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func getCommands() map[string]cliCommand {
+	return map[string]cliCommand{
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+	}
 }
